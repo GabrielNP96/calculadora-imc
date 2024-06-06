7b22f285a278e809b8e21eeac479d06438668a20
GabrielNP96 <gabrielnp1996@gmail.com>
2024-06-06 05:04:30 -0300

    começando ojs

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-let 
+let weight = document.querySelector('#weight').value;
+let height = document.querySelector('#height').value;
+const btnSend = document.querySelector('#btn-send');
+const inputField = document.getElementById('myInput');
+
