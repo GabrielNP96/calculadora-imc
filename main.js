8bfd38cc6c94a209c9e7aae3abcbb19ed6294b31
GabrielNP96 <gabrielnp1996@gmail.com>
2024-06-07 05:11:59 -0300

    alterações

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
-let weight = document.querySelector('#weight').value;
-let height = document.querySelector('#height').value;
-const btnSend = document.querySelector('#btn-send');
-const inputField = document.getElementById('myInput');
+import { convertToNumber } from "./modules/convertToNumber.js";
+
+const btnSend = document.querySelector('#btn-send')
+
+btnSend.addEventListener('click', () => {
+    let weight = document.querySelector('#weight').value
+        weight = weight.replace(/\,/g, '.')
+    let height = document.querySelector('#height').value
+        height = height.replace(/\,/g, '.')
+
+    userData = convertToNumber(weight, height)//retorna um array na mesma ordem com as strings convertidas em números
+
+
+
+    
+
+    console.log(typeof(userData[0]))
+    console.log(typeof(userData[1]))
+})
+
 
---
+++ modules/convertToNumber.js
@@ -0,0 +1,7 @@
+export default function convertToNumber(weight, height) {
+   const userWeight = Number(weight)
+   const userHeight = Number(height)
+
+   return [userWeight, userHeight]
+}
+
